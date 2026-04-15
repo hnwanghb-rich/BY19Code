26e8334a88abd54257619f5421013629e2447c35
hnwanghb-rich <hnwanghb@Gmail.com>
2026-04-15 19:02:23 +0800

    chore: 清理非BY19Code项目的临时文件和测试脚本

--- ll.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-print("我的最爱：深度研究玩透重庆小花园")
--- test_ls.py
+++
@@ -1,5 +0,0 @@
-import os
-print("当前目录:", os.getcwd())
-print("文件列表:")
-for item in os.listdir('.'):
-    print(f"  {item}")
